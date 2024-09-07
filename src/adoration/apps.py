913f1600a68fe5404e98c50054505046649ca23e
Piotr Tutak <p.tutak@outlook.com>
2024-09-07 19:51:32 +0200

    fixing

--- src/adoration/apps.py
+++ src/adoration/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AdorationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adoration'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "adoration"
